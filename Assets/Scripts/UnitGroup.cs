e369080a5717cb06ef7250676ff13b1fc9e0f7bd
Christopher Clarke <dark_sparky@hotmail.com>
2022-10-19 15:57:41 -0400

    added a bit more to do with groups

--- Assets/Scripts/UnitGroup.cs
+++ Assets/Scripts/UnitGroup.cs
@@ -1,26 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class UnitGroup : MonoBehaviour
 {
-    public GameObject leader;
+    /// <summary> The leader is the transform the group will follow. </summary>
+    public Transform hiddenLeader;
+    
+    /// <summary> playerUnit is the Unit that the player is in control of - this isn't always in the UnitGroup</summary>
+    public GameObject unitLeader;
+    
+    /// <summary> List of units in the group </summary>
     public List<GameObject> units = new List<GameObject>();
-    public UnitGroup(GameObject leader) {
-        this.leader = leader;
-        units.Add(leader);
+    
+    /// <summary>
+    /// Create a new unit group class with a hiddenLeader. 
+    /// </summary>
+    /// <param name="hiddenLeader">The hiddenLeader is the transorm the group will follow.</param>
+    /// <param name="unitLeader">The unit that is designated as Leader</param>
+    public UnitGroup(Transform hiddenLeader, GameObject unitLeader) {
+        this.hiddenLeader = hiddenLeader;
+        this.unitLeader = unitLeader;
+        AddUnit(unitLeader);
     }
     
-
-    // Start is called before the first frame update
-    void Start()
+    ~UnitGroup()
     {
-        
+        units.Clear();
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Adds a unit to the UnitGroup if it is not already in the group.
+    /// </summary>
+    /// <param name="toAdd">The unit you are adding to the group</param>
+    public void AddUnit(GameObject toAdd)
+    {
+        if (!units.Contains(toAdd))
+        {
+            units.Add(toAdd);
+        }
+    }
+    /// <summary>
+    /// Removes a unit to the UnitGroup if it is in the group.
+    /// </summary>
+    /// <param name="toRemove">The unit you are removing from the group</param>
+    public void RemoveUnit(GameObject toRemove)
+    {
+        if (units.Contains(toRemove))
+        {
+            units.Remove(toRemove);
+        }
+    }
+
     void Update()
     {
-        
+        if (unitLeader)
+        {
+            UpdateLeaderPosition();
+            UpdateUnitDestinations();
+        }
+         
     }
+
+    void UpdateUnitDestinations()
+    {
+        foreach(GameObject item in units)
+        {
+            if (item != unitLeader)
+            {
+                item.GetComponent<NavMeshAgent>().destination = hiddenLeader.position;
+            }
+            
+        }
+    }
+
+    void UpdateLeaderPosition()
+    {
+        hiddenLeader.position = unitLeader.transform.position;
+    }
+
+    
+
+
 }
