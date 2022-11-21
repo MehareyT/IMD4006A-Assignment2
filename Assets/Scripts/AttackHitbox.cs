b44b81be4cdc3741d86619bceae8d2d3768bbfef
Snuzfuz <jamie.hopkins@sympatico.ca>
2022-11-21 12:25:34 -0500

    GUI changes pt.2

--- Assets/Scripts/AttackHitbox.cs
+++ Assets/Scripts/AttackHitbox.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class AttackHitbox : MonoBehaviour
 {
     public GameObject deathEffect;
+    public PopulationManager popManager;
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player"){
             //damage
+            popManager.Hurt(1);
             Instantiate(deathEffect,other.transform.position,other.transform.rotation);
             Destroy(other.gameObject);
         }
--- Assets/Scripts/PopulationManager.cs
+++ Assets/Scripts/PopulationManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PopulationManager : MonoBehaviour
 {
@@ -9,15 +10,40 @@ public class PopulationManager : MonoBehaviour
 
     public int maxPopulation = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public int popRemoved = 0;
+
+    public Transform spawnPoint;
+
+    public TMP_Text populationText;
+
+    public GameObject numberPrefab;
+
+    public Animator pop;
+
+    float counting = 0f;
+
+    public void Hurt(int x){
+        if(counting <= 0){
+            counting = 0.25f;
+        }
+        popRemoved += x;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        populationText.text = population.ToString();
+        if(counting > 0){
+            counting -= Time.deltaTime;
+        }
+        else if(popRemoved > 0){
+            pop.SetTrigger("Population");
+
+            var obj = Instantiate(numberPrefab,spawnPoint.position,Quaternion.identity,spawnPoint);
+            obj.GetComponent<TMP_Text>().text = "-"+popRemoved.ToString();
+            Destroy(obj,1.5f);
+            population -= popRemoved;
+            popRemoved = 0;
+            
+
+        }
     }
 }
