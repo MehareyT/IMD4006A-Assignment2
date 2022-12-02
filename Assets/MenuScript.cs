f4f268806705e71d74ccc4f9c402ffd9dc52ad00
Snuzfuz <jamie.hopkins@sympatico.ca>
2022-12-02 17:39:04 -0500

    MenuBasics

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class MenuScript : MonoBehaviour
 {
     public void BackToMenu(){
-        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
 }
